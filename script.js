34afd63894774d31cbc52928b00aaa3fe6e0b0df
iStubsy <63608655+iStubsy@users.noreply.github.com>
2024-03-27 20:57:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ window.onscroll = function () {
         document.querySelector(".navigationBar").classList.add("navigationBarScrolled");
         document.querySelector(".navigationBar_inner").classList.add("navigationBar-innerScrolled");
         document.querySelector(".navigationBar_logoText").classList.add("navigationBar_logoText-scrolled");
+
+        document.querySelector(".backToTop").classList.add("backToTopScrolled");
     } else {
         document.querySelector(".navigationBar").classList.remove("navigationBarScrolled");
         document.querySelector(".navigationBar_inner").classList.remove("navigationBar-innerScrolled");
         document.querySelector(".navigationBar_logoText").classList.remove("navigationBar_logoText-scrolled");
+
+        document.querySelector(".backToTop").classList.remove("backToTopScrolled");
     }
+}
+
+
+document.querySelector(".backToTop").onclick = function () {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
 }
