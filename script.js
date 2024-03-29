fddff2fcb6af295dfcaef1f054c7fc8fe5b7f127
iStubsy <63608655+iStubsy@users.noreply.github.com>
2024-03-29 22:02:24 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+/***************** OVERLAY *****************/
+
+document.querySelector(".hamburgerMenu").onclick = function () {
+    document.querySelector(".overlay").classList.add("overlayShow");
+    document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
+
+    //document.querySelector(".hmlTransform1").style.transform = "rotate(45deg)";
+    //document.querySelector(".hmlTransform2").style.transform = "rotate(-45deg)";
+
+    //moveUpRevealFunOverlay();
+    /* for (var y = 0; y < document.querySelectorAll(".overlayAnimation").length; y++) {
+        for (var x = 0; x < document.querySelectorAll(".overlayAnimation")[y].children.length; x++) {
+            //document.querySelectorAll(".overlayAnimation")[x].getElementsByTagName("*")[x].style.opacity = "1";
+            //console.log(document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*").length);
+
+            //document.querySelectorAll(".overlayAnimation")[y].children[x].classList.add("tro");
+            //document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*")[x].style.transition = "1s";
+            //document.querySelectorAll(".overlayAnimation")[y].children[x].style.transitionDelay = x / 4 + "s";
+
+            //document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*")[x].style.border = "solid 2px green";
+            //console.log(document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*").length);
+        }
+
+    } */
+
+
+    for (var i = 0; i < document.querySelectorAll(".hamburgerMenuLine").length; i++) {
+        //document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "8px auto";
+    }
+}
+
+document.querySelector(".closeButton").onclick = function () {
+    document.querySelector(".overlay").classList.remove("overlayShow");
+    document.getElementsByTagName("HTML")[0].style.overflow = "auto";
+}
+
+
 window.onscroll = function () {
     if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
         document.querySelector(".navigationBar").classList.add("navigationBarScrolled");
@@ -19,45 +56,4 @@ document.querySelector(".backToTop").onclick = function () {
     document.querySelector("html").style.scrollBehavior = "smooth";
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0;
-}
-
-
-
-
-
-
-/***************** OVERLAY *****************/
-
-document.querySelector(".hamburgerMenu").onclick = function () {
-    document.querySelector(".overlay").classList.add("overlayShow");
-    document.getElementsByTagName("HTML")[0].style.overflow = "hidden";
-
-    //document.querySelector(".hmlTransform1").style.transform = "rotate(45deg)";
-    //document.querySelector(".hmlTransform2").style.transform = "rotate(-45deg)";
-
-    //moveUpRevealFunOverlay();
-    for (var y = 0; y < document.querySelectorAll(".overlayAnimation").length; y++) {
-        for (var x = 0; x < document.querySelectorAll(".overlayAnimation")[y].children.length; x++) {
-            //document.querySelectorAll(".overlayAnimation")[x].getElementsByTagName("*")[x].style.opacity = "1";
-            //console.log(document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*").length);
-
-            document.querySelectorAll(".overlayAnimation")[y].children[x].classList.add("tro");
-            //document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*")[x].style.transition = "1s";
-            document.querySelectorAll(".overlayAnimation")[y].children[x].style.transitionDelay = x / 4 + "s";
-
-            //document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*")[x].style.border = "solid 2px green";
-            //console.log(document.querySelectorAll(".overlayAnimation")[y].getElementsByTagName("*").length);
-        }
-
-    }
-
-
-    for (var i = 0; i < document.querySelectorAll(".hamburgerMenuLine").length; i++) {
-        //document.querySelectorAll(".hamburgerMenuLine")[i].style.margin = "8px auto";
-    }
-}
-
-document.querySelector(".closeButton").onclick = function () {
-    document.querySelector(".overlay").classList.remove("overlayShow");
-    document.getElementsByTagName("HTML")[0].style.overflow = "auto";
 }
